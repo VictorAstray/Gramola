448538f0f2401311cbc8afc79e90301bab223f87
VictorAstray <victor.astray@alu.uclm.es>
2026-01-09 17:34:10 +0100

    commit

--- gramolafe/src/main.ts
+++ gramolafe/src/main.ts
@@ -1,6 +1,7 @@
+import 'zone.js';  // <--- 1. Importamos la pieza que faltaba
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component'; // <--- CAMBIADO (.component)
+import { AppComponent } from './app/app.component'; // <--- 2. Corregimos la ruta (añadido .component)
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
